cabfb7b79b37f3a279a94319c23bf2086ee16464
Leland Byrd <leland@integral-ed.com>
2024-02-26 20:13:59 -0600

    added or statement to access production env

--- src/helpers/airtablePost.js
+++ src/helpers/airtablePost.js
@@ -1,5 +1,5 @@
 import Airtable from "airtable";
-const apiKey = import.meta.env.VITE_AIRTABLE_API_KEY;
+const apiKey = import.meta.env.VITE_AIRTABLE_API_KEY || process.env.VITE_AIRTABLE_API_KEY;
 const timeSheetHoursBase = "apps7roRhnziLR2ou"
 
 let base = new Airtable({apiKey: `${apiKey}`}).base(`${timeSheetHoursBase}`)
