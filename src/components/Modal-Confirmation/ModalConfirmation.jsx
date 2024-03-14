93a98dda6e625408284afaecc07f269545cd9dc7
Leland Byrd <leland@integral-ed.com>
2024-03-14 18:22:09 -0500

    formatted time in confirm-modal

--- src/components/Modal-Confirmation/ModalConfirmation.jsx
+++ src/components/Modal-Confirmation/ModalConfirmation.jsx
@@ -11,22 +11,39 @@ export default function ModalConfirmation({
     jobcode3,
     notes,
     handleModalClose
-}) {
+}) 
+{
+    const [newStartTime, setNewStartTime] = useState()
+    const [newEndTime, setNewEndTime] = useState()
+
+    useEffect(() => {
+        const [hours, minutes] = startTime.split(":").map(Number)
+        const AMorPM = hours > 12 ? "PM": "AM"
+        const format12hr = hours%12 || 12
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        setNewStartTime(`${format12hr}:${formattedMinutes} ${AMorPM}`)
+
+    }, [startTime])
+
+    useEffect(() => {
+        setNewEndTime(`${endTime.replace(/^0+/, "")}`)
+    }, [endTime])
+
     if (!showModal) return null
 
     return (
         <>
             <div>
 
-                <div className='modal'onClick={handleModalClose}>
+                <div className='modal' onClick={handleModalClose}>
                     <div className='modal-card' onClick={e => e.stopPropagation()}></div>
                     {/* replace with a .svg of close button */}
-                    <div className='modal-content'onClick={e => e.stopPropagation()}>
+                    <div className='modal-content' onClick={e => e.stopPropagation()}>
                         <p>Name: {userName}</p>
                         <p>Date: {date} </p>
                         <p>Duration: {duration} </p>
-                        <p>Start Time: {startTime} </p>
-                        <p>End Time: {endTime} </p>
+                        <p>Start Time: {newStartTime} </p>
+                        <p>End Time: {newEndTime} </p>
                         <p>Job-Code/Product: {jobcode3} </p>
                         <p>Notes on Entry: {notes} </p>
                         <p>Your time entry has been submitted!</p>
